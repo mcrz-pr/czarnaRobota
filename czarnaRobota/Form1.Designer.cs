a9e68caf36d43037ba5f57256fc91a3dd2482b89
student <student@s001-pc12>
2022-10-11 08:26:06 +0200

    Ostateczne

--- czarnaRobota/Form1.Designer.cs
+++ czarnaRobota/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.maskedPesel = new System.Windows.Forms.MaskedTextBox();
@@ -495,6 +496,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
